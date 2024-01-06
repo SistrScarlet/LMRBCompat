9d79ae507180062ed6c1a211a7fe8ed2c7272049
sistr <game.sistr@gmail.com>
2024-01-06 15:02:19 +0900

    1.20.2変更点修正

--- common/src/main/java/net/sistr/lmrbcompat/client/config/ConfigHubScreen.java
+++ common/src/main/java/net/sistr/lmrbcompat/client/config/ConfigHubScreen.java
@@ -52,7 +52,7 @@ public class ConfigHubScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderBackground(context);
+        renderBackground(context, mouseX, mouseY, delta);
         super.render(context, mouseX, mouseY, delta);
     }
 
